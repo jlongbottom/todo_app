919a6552d3d402d2357cda6fea8c958d6a7724a2
Justin Longbottom <jlongbottom@gmail.com>
2025-01-17 16:00:55 -0500

    Fetch and display to do tasks from the server

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,28 @@
-import logo from "./logo.svg";
+import { useQuery } from "@apollo/client";
+import { GET_TASKS } from "./queries";
+
 import "./App.css";
 
-function App() {
+const App = () => {
+  const { loading, error, data } = useQuery(GET_TASKS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>To-Do List</h1>
+      <ul>
+        {data.tasks.map((task) => (
+          <li key={task.id}>
+            <h2>{task.title}</h2>
+            <p>{task.description}</p>
+            <p>Completed: {task.completed ? "Yes" : "No"}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
